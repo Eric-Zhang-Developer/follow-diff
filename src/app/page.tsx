1fb87beeebaa663b07d84dd1b724d335aaec21c6
Eric Zhang <superezzhang@gmail.com>
2025-06-14 21:50:22 -0400

    add icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Github, Upload } from "lucide-react";
 import Link from "next/link";
 
 
@@ -11,14 +12,15 @@ export default function Home() {
         <h2 className="text-xl text-center">Safely see your non-followers using your official Instagram data. Your files are processed right here in your browser and are never uploaded anywhere.</h2>
         {/* Drop Zone */}
         <div className="border-2 px-1/2 py-40 flex flex-col gap-8 items-center justify-center w-1/2">
+          <Upload size={50}></Upload>
           Drag & drop your followers.json & following.json files here
           <span>or</span>
           <button className="border-1 p-2">Select Files</button>
         </div>
         <Link href="/tutorial" className="underline"> Don&apos;t have your files? Here&apos;s how to get them.</Link>
       </main>
-      <footer className="fixed bottom-0 right-0 p-4">
-        Open Source & Privacy-First
+      <footer className="fixed bottom-0 right-0 p-5 flex flex-row gap-2">
+        Open Source & Privacy-First <Github></Github>
       </footer>
     </div>
   );
