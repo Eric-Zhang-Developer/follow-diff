5bd39a8ad22c95e7f716165ba18893dc483873d5
Eric Zhang <superezzhang@gmail.com>
2025-06-14 21:47:11 -0400

    remove followdiff element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="mx-auto container p-2">
-      <header className="text-3xl text-center mb-8">FollowDiff</header>
+    <div className="mx-auto container p-10">
       <main className="flex items-center justify-center flex-col gap-4">
-        <h1 className="text-2xl">Curious Who Doesn&apos;t Follow You Back?</h1>
+        <h1 className="text-4xl">Curious Who Doesn&apos;t Follow You Back?</h1>
         <h2 className="text-xl text-center">Safely see your non-followers using your official Instagram data. Your files are processed right here in your browser and are never uploaded anywhere.</h2>
         {/* Drop Zone */}
         <div className="border-2 px-1/2 py-40 flex flex-col gap-8 items-center justify-center w-1/2">
@@ -19,7 +18,7 @@ export default function Home() {
         <Link href="/tutorial" className="underline"> Don&apos;t have your files? Here&apos;s how to get them.</Link>
       </main>
       <footer className="fixed bottom-0 right-0 p-4">
-        Open Source & Privacy-First 
+        Open Source & Privacy-First
       </footer>
     </div>
   );
