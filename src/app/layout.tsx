3ef3d39f78b8c6a9dc87b6aa6fabe03b84436513
Eric Zhang <superezzhang@gmail.com>
2025-06-18 18:49:22 -0400

    add basic metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import "./globals.css";
 
 
+export const metadata = {
+  title: "Follower Diff - See Who Doesn't Follow You Back",
+  description:
+    "Safely see your Instagram non-followers using your official Instagram data. Your files are processed right in your browser and never uploaded anywhere.",
+}
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
