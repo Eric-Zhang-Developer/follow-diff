2205b1fe7b36e629b0c9a3610a1503018f99454c
Eric Zhang <superezzhang@gmail.com>
2025-06-23 11:26:21 -0400

    test: two empty arrays

--- src/lib/__tests__/comparison.test.ts
+++ src/lib/__tests__/comparison.test.ts
@@ -2,11 +2,18 @@ import Compare from "../comparison";
 
 describe('Comparison Tests', () => {
   it('should return an array of users who are in the following list but not the followers list', () => {
-
     const followers = ["Alice", "Brenda"];
     const following = ["Alice", "Brenda", "Charlie"];
     const result = Compare(following, followers);
 
     expect(result).toEqual(["Charlie"]); 
   });
+
+  it('should return an empty array when both lists are empty' , () => {
+    const followers = [""];
+    const following = [""];
+    const result = Compare(following, followers);
+
+    expect(result).toEqual([]);
+  })
 });
