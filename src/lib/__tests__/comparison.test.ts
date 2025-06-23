6f4c39567478b8e7789b3cab864eb292df8769d1
Eric Zhang <superezzhang@gmail.com>
2025-06-23 12:26:16 -0400

    test: add more user logic tests, fix empty strings to be empty

--- src/lib/__tests__/comparison.test.ts
+++ src/lib/__tests__/comparison.test.ts
@@ -11,15 +11,15 @@ describe("Comparison Tests", () => {
     });
 
     it("should return an empty array when both lists are empty", () => {
-      const followers = [""];
-      const following = [""];
+      const followers: string[] = [];
+      const following: string[] = [];
       const result = Compare(following, followers);
 
       expect(result).toEqual([]);
     });
 
     it("should return the entire following list if there are not followers to compare to", () => {
-      const followers = [""];
+      const followers: string[] = [];
       const following = ["Alice", "Brenda", "Charlie", "David"];
       const result = Compare(following, followers);
 
@@ -36,6 +36,29 @@ describe("Comparison Tests", () => {
       expect(result).toEqual(["ex_friend"]);
     });
 
-    it("should return only ex-friend when my ex-friend unfollows me and I follow brand / celerity accounts I don't expect to follow me back", () => {});
+    it("should return only celebrities that I expect when no one unfollows me", () => {
+      const followers = ["friend", "friend_2"];
+      const following = ["friend", "friend_2", "POTUS", "Tom_Cruise"];
+      const result = Compare(following, followers);
+
+      expect(result).toEqual(["POTUS", "Tom_Cruise"]);
+    });
+
+    it("should return no accounts when I don't follow accounts who follow me", () => {
+      const followers = ["friend", "friend_2", "spam_bot"];
+      const following = ["friend", "friend_2"];
+      const result = Compare(following, followers);
+
+      expect(result).toEqual([]);
+    });
+
+    it("should return ex-friend AND celebrity accounts when my ex-friend unfollows me and I follow brand / celerity accounts I don't expect to follow me back", () => {
+      const followers = ["friend"];
+      const following = ["friend", "ex_friend", "POTUS", "Tom_Cruise"];
+      const result = Compare(following, followers);
+
+      expect(result).toEqual(["ex_friend", "POTUS", "Tom_Cruise"]);
+    });
+
   });
 });
