fdb3d585f0c3ee3e086483f483dd135f64418a6c
Eric Zhang <superezzhang@gmail.com>
2025-06-27 22:47:07 -0400

    feature: use Zod to add runtime type safety

--- src/lib/extractNamesFromJson.ts
+++ src/lib/extractNamesFromJson.ts
@@ -1,19 +1,6 @@
-// The reason for these interfaces is so that typescript neatly throws an error when a user 
-// adds a json that is not a followers.json or following.json format 
+import { FollowingList, FollowerList } from "./types";
 
-interface ExpectedJSON {
-  title: string;
-  media_list_data: unknown[];
-  string_list_data: stringListData[];
-}
-
-interface stringListData{
-  href: string;
-  value: string;
-  timestamp: number;
-}
-
-export default function ExtractNamesFromJson(json: ExpectedJSON[]) {
+export default function ExtractNamesFromJson(json: FollowingList | FollowerList) {
   const usernames = json.map(user => user.string_list_data[0].value);
   return usernames;
 }
