4db122c46d4693a58ef0275612ba1f9b1dea4186
Eric Zhang <superezzhang@gmail.com>
2025-07-08 16:41:53 -0400

    documentation: run tailwind prettier config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,21 @@
 import "./globals.css";
-import { Inter, Lora } from "next/font/google"
-
+import { Inter, Lora } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
-})
+});
 
 const lora = Lora({
-  subsets: ['latin'],
-  variable: "--font-lora"
-})
+  subsets: ["latin"],
+  variable: "--font-lora",
+});
 
 export const metadata = {
   title: "Follower Diff - See Who Doesn't Follow You Back",
   description:
     "Safely see your Instagram non-followers using your official Instagram data. Your files are processed right in your browser and never uploaded anywhere.",
-}
-
+};
 
 export default function RootLayout({
   children,
@@ -26,10 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${lora.variable}`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.variable} ${lora.variable}`}>{children}</body>
     </html>
   );
 }
