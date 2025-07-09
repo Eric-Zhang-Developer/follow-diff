f3c30e148cf540751bcd76f53f5f101724910670
Eric Zhang <superezzhang@gmail.com>
2025-07-09 18:22:42 -0400

    style: container for tutorial page and Go Back button

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -1,10 +1,14 @@
-import Link from "next/link"
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 
 export default function Tutorial() {
   return (
-    <main className="mx-auto container flex flex-col items-center">
-      <h1 className="text-xl">Tutorial Page</h1>
-      <Link href="/" className="text-lg bg-slate-300 p-4 border-2"> Go Back</Link>
-    </main>
+    <div className="font-text flex min-h-screen flex-col px-2 pt-5 pb-4 md:px-4 md:pt-10">
+      <main className="container mx-auto flex flex-grow flex-col items-center gap-4 px-2 md:px-12">
+        <Link href="/" className="p-4 text-lg self-start inline-flex items-center"><ArrowLeft></ArrowLeft> Go Back</Link>
+          
+        <h1 className="text-xl">Tutorial Page</h1>
+      </main>
+    </div>
   );
 }
