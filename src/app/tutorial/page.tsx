450572073f86899685ad43b401a0baf7e5c85fd0
Eric Zhang <superezzhang@gmail.com>
2025-07-09 18:31:52 -0400

    feat: step 1

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -3,7 +3,7 @@ import { ArrowLeft } from "lucide-react";
 
 export default function Tutorial() {
   return (
-    <div className="font-text flex min-h-screen flex-col px-2 pt-5 pb-4 md:px-4 md:pt-10 text-primary">
+    <div className="font-text text-primary flex min-h-screen flex-col px-2 pt-5 pb-4 md:px-4 md:pt-10">
       <main className="container mx-auto flex flex-grow flex-col items-start gap-4 px-2 md:px-12">
         <Link
           href="/"
@@ -12,10 +12,33 @@ export default function Tutorial() {
           <ArrowLeft></ArrowLeft> Go Back
         </Link>
 
-        <h1 className="font-main text-3xl md:text-5xl ">
+        <h1 className="font-main text-3xl md:text-5xl">
           How to Get Your Instagram Data Files
         </h1>
-        
+
+        <section className="space-y-4">
+          <h2 className="text-2xl font-main ">Step 1</h2>
+          <ol className="list-decimal space-y-2 pl-6">
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna
+            </li>
+          </ol>
+        </section>
+
+
       </main>
     </div>
   );
