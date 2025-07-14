adc7d5c31b365fcd4d18c19ed4c3f4cf300ba655
Eric Zhang <superezzhang@gmail.com>
2025-07-14 17:48:06 -0400

    feat: pro tip section

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -91,6 +91,14 @@ export default function Tutorial() {
         </section>
 
         <section className="bg-light-accent border-accent space-y-2 rounded-3xl border-1 px-4 py-6 shadow-md">
+          <p className="font-main text-2xl font-semibold">Pro Tip - Finding Your Files</p>
+          <p className="text-secondary text-lg">
+            When you go back to upload, your device&apos;s file browser will open. The folder you just unzipped can be tricky to find.
+          </p>
+          <p className="text-secondary text-lg">The easiest way to locate it is to use the search bar and type <strong>followers_and_following</strong>. This will take you directly to the folder containing the files required</p>
+        </section>
+
+        <section className="bg-light-accent border-accent space-y-2 rounded-3xl border-1 px-4 py-6 shadow-md">
           <p className="font-main text-2xl font-semibold">Privacy Note</p>
           <p className="text-secondary text-lg">
             Remember, this application processes these files entirely in your
